de1994356aa520662bb92bb110d7b53678233d56
Márton Áron <aron17marton@gmail.com>
2024-11-16 22:25:08 +0100

    migration: from Java to Typescript

--- algorithm/src/main/java/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        var gettingBitches = new GettingBitchesImpl();
-        gettingBitches.run();
-        System.out.println(gettingBitches);
-    }
-}
